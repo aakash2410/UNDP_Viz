775b6094d74aed453c14600c40fdcd130c41660d
Aakash Sangani <aakashsangani@Aakashs-MacBook-Air.local>
2026-03-10 15:50:08 +0530

    remove kalpa logo

--- src/components/dashboard/MacroOverview.tsx
+++ src/components/dashboard/MacroOverview.tsx
@@ -34,10 +34,10 @@ export default function MacroOverview() {
                     </div>
                     <div className="border-r border-slate-300 px-6 md:px-10 flex items-center justify-center">
                         <img src="/artha-logo.png" alt="Artha Global Logo" className="h-10 md:h-14 w-auto object-contain" />
-                    </div>*/}
+                    </div>
                     <div className="pl-6 md:pl-10 flex items-center justify-center">
                         <img src="/kalpa-logo.png" alt="Kalpa Impact Logo" className="h-10 md:h-14 w-auto object-contain" />
-                    </div>
+                    </div>*/}
 
                 </div>
                 <div className="inline-flex items-center justify-center space-x-2 bg-blue-100 text-blue-800 px-6 py-2 rounded-full text-base font-semibold mb-8">
